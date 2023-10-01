2d917e2f1431c39da55a84ac3dee3195fc64a7c2
devadonis <devadonis00@gmail.com>
2023-10-01 20:14:44 +0300

    part-02-url-path-parameters

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI, APIRouter
 
+
+RECIPES = [
+    {
+        "id": 1,
+        "label": "Chicken Vesuvio",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html",
+    },
+    {
+        "id": 2,
+        "label": "Chicken Paprikash",
+        "source": "No Recipes",
+        "url": "http://norecipes.com/recipe/chicken-paprikash/",
+
+    },
+    {
+        "id": 3,
+        "label": "Cauliflower and Tofu Curry Recipe",
+        "source": "http://www.seriouseats.com/recipes/2011/02/cauliflower-and-tofu-curry-recipe.html",
+    }
+]
+
+
 app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 
 api_router = APIRouter()
+
 
 @api_router.get("/", status_code=200)
 def root() -> dict:
@@ -11,7 +35,22 @@ def root() -> dict:
     """
     return {"msg": "Hello World!"}
 
+
+# New addition, path parameter
+# https://fastapi.tiangolo.com/tutorial/path-params/
+@api_router.get("/recipe/{recipe_id}", status_code=200)
+def fetch_recipe(*, recipe_id: int) -> dict:
+    """
+    Fetch a single recipe by ID
+    """
+
+    result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
+    if result:
+        return result[0]
+
+
 app.include_router(api_router)
+
 
 if __name__ == "__main__":
     # Use this for debugging purpose only
