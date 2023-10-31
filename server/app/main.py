8809ccf88b784a23b9d4a12b12252b42fdb7b56a
devadonis <devadonis00@gmail.com>
2023-10-31 06:01:13 +0300

    part-07 Database setup with sqlalchemy and alembic

--- server/app/main.py
+++ server/app/main.py
@@ -1,13 +1,17 @@
-from fastapi import FastAPI, APIRouter, Query, HTTPException, Request
+from fastapi import FastAPI, APIRouter, Query, HTTPException, Request, Depends
 from fastapi.templating import Jinja2Templates
 
-from typing import Optional
+from typing import Optional, Any
 from pathlib import Path
+from sqlalchemy.orm import Session
 
-from app.schemas import RecipeSearchResults, Recipe, RecipeCreate
-from app.recipe_data import RECIPES
+from app.schemas.recipe import RecipeSearchResults, Recipe, RecipeCreate
+from app import dependencies
+from app import crud
 
 
+# Project Directories
+ROOT = Path(__file__).resolve().parent.parent
 BASE_PATH = Path(__file__).resolve().parent
 TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
 
@@ -20,64 +24,58 @@ api_router = APIRouter()
 # https://www.starlette.io/templates/
 # https://jinja.palletsprojects.com/en/3.0.x/templates/#synopsis
 @api_router.get("/", status_code=200)
-def root(request: Request) -> dict:
+def root(request: Request, db: Session = Depends(dependencies.get_db)) -> dict:
     """
     Root GET
     """
+    recipes = crud.recipe.get_multi(db=db, limit=10)
     return TEMPLATES.TemplateResponse(
         "index.html",
-        {"request": request, "recipes": RECIPES}
+        {"request": request, "recipes": recipes}
     )
 
 
 @api_router.get("/recipe/{recipe_id}", status_code=200, response_model=Recipe)
-def fetch_recipe(*, recipe_id: int) -> dict:
+def fetch_recipe(*, recipe_id: int, db: Session = Depends(dependencies.get_db)) -> Any:
     """
     Fetch a single recipe by ID
     """
-    result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
+    result = crud.recipe.get(db=db, id=recipe_id)
     if not result:
+        # The exception is raised, not returned, - you will get a validation error otherwise
         raise HTTPException(
             status_code=404, detail=f"Recipe with ID {recipe_id} not found"
         )
 
-    return result[0]
+    return result
 
 
 @api_router.get("/search/", status_code=200, response_model=RecipeSearchResults)
 def search_recipes(
     *,
     keyword: Optional[str] = Query(None, min_length=3, examples=["chicken"]),
-    max_results: Optional[int] = 10
+    max_results: Optional[int] = 10,
+    db: Session = Depends(dependencies.get_db)
 ) -> dict:
     """
     Search for recipes based on label keyword
     """
+    recipes = crud.recipe.get_multi(db=db, limit=max_results)
     if not keyword:
-        # we use Python list slicing to limit results
-        # based on the max_results query parameter
-        return {"results": RECIPES[:max_results]}
+        return {"results": recipes}
 
-    results = filter(lambda recipe: keyword.lower()
-                     in recipe["label"].lower(), RECIPES)
+    results = filter(lambda recipe: keyword.lower() in recipe["label"].lower(), recipes)
     return {"results": list(results)[:max_results]}
 
 
 @api_router.post("/recipe/", status_code=201, response_model=Recipe)
-def create_recipe(*, recipe_in: RecipeCreate) -> dict:
+def create_recipe(*, recipe_in: RecipeCreate, db: Session = Depends(dependencies.get_db)) -> dict:
     """
     Create a new recipe (in memory only)
     """
-    new_entry_id = len(RECIPES) + 1
-    recipe_entry = Recipe(
-        id=new_entry_id,
-        label=recipe_in.label,
-        source=recipe_in.source,
-        url=recipe_in.url,
-    )
-    RECIPES.append(recipe_entry.model_dump())
+    recipe = crud.recipe.create(db=db, obj_in=recipe_in)
 
-    return recipe_entry
+    return recipe
 
 
 app.include_router(api_router)
--- server/run.sh
+++ server/run.sh
@@ -1,4 +1,4 @@
-# !/bin/sh
+#!/bin/sh
 
 export APP_MODULE=${APP_MODULE-app.main:app}
 export HOST=${HOST:-0.0.0.0}
