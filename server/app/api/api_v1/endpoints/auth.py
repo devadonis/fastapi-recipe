be9496c4d1f254cf908d528ba7666f6a67a98f3f
devadonis <devadonis00@gmail.com>
2024-02-15 17:05:49 +0200

    fix code

--- server/app/api/api_v1/endpoints/auth.py
+++ server/app/api/api_v1/endpoints/auth.py
@@ -16,13 +16,15 @@ router = APIRouter()
 @router.post("/login")
 def login(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()) -> Any:
     """
-    Get the JWT for a user with dta from OAuth2 request form body
+    Get the JWT for a user with data from OAuth2 request form body
     """
     user = authenticate(email=form_data.username, password=form_data.password, db=db)
 
     if not user:
         raise HTTPException(
-            status_code=400, detail="Incorrect username or password")
+            status_code=400,
+            detail="Incorrect username or password"
+        )
 
     return {
         "access_token": create_access_token(sub=user.id),
